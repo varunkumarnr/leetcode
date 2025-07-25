2f969bd69ec6ca4de49e21b8964b95a4a95e5e05
Varun Kumar N.R <varunnrk@gmail.com>
2025-07-25 08:11:22 +0530

    Time: 1 ms (100%), Space: 42.4 MB (78.44%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSum(int[] nums) {
+        int sum =0;
+        Arrays.sort(nums);
+        int prev = 0;
+        if(nums[nums.length-1] <= 0) {
+            return nums[nums.length-1];
+        }
+        for(int i = 0; i<nums.length; i++) {
+            if(nums[i] != prev && nums[i] >0) {
+                sum += nums[i];
+                prev = nums[i];
+            }
+        }
+        return sum; 
+    }
+        
+}
