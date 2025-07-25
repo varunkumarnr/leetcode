1bf1cc9e0fadd6f1218441a883d4884bebfda2fb
Varun Kumar N.R <varunnrk@gmail.com>
2025-07-25 16:27:38 +0530

    Time: 0 ms (100%), Space: 40.8 MB (10.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,3 +1,5 @@
+
+// ------------------ pure recursion --------------------------
 // class Solution {
 //     int count = 0; 
 //     public int climbStairs(int n) {
@@ -17,20 +19,33 @@
 //     }
 // }
 
-
-class Solution { 
-    HashMap<Integer, Integer> memo = new  HashMap<>();
-    public int climbStairs(int n) { 
-       return climb(n);
-    }
-    int climb(int n) {
-        if(memo.containsKey(n)) return memo.get(n); 
+// ---------------------- top down ------------------------
+// class Solution { 
+//     HashMap<Integer, Integer> memo = new  HashMap<>();
+//     public int climbStairs(int n) { 
+//        return climb(n);
+//     }
+//     int climb(int n) {
+//         if(memo.containsKey(n)) return memo.get(n); 
         
-        if(n==0 || n== 1){
-            return 1; 
+//         if(n==0 || n== 1){
+//             return 1; 
+//         }
+//         int result = climb(n-1) + climb(n-2); 
+//         memo.put(n, result);
+//         return result; 
+//     }
+// }
+
+// -------------------- bottom up ------------------------
+class Solution {
+    public int climbStairs(int n) {
+        int[] steps = new int[n+1]; 
+            steps[0] = 1;
+            steps[1] = 1;  
+        for (int i = 2; i <=n; i++) {
+            steps[i] = steps[i-1] + steps[i-2];   
         }
-        int result = climb(n-1) + climb(n-2); 
-        memo.put(n, result);
-        return result; 
+        return steps[n];
     }
 }
