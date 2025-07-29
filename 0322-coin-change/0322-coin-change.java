54cccf474026d4faf37f172f91488da0732a4196
Varun Kumar N.R <varunnrk@gmail.com>
2025-07-29 12:58:11 +0530

    Time: 152 ms (5%), Space: 47 MB (5.76%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,26 @@
+class Solution {
+    HashMap<Integer, Integer> memo = new HashMap<>();
+    public int coinChange(int[] coins, int amount) {
+        int result = dfs(coins, amount, 0, 0); 
+        return result == Integer.MAX_VALUE ? -1 : result ;
+    }
+
+    int dfs(int[] coins, int amount, int start, int curr) {
+        if(amount == 0) {
+            return 0; 
+        }
+        if(amount < 0) {
+            return Integer.MAX_VALUE; 
+        }
+        if (memo.containsKey(amount)) return memo.get(amount);
+        int min = Integer.MAX_VALUE;
+        for(int i= 0; i<coins.length; i++) {
+           int res =  dfs(coins, amount - coins[i], i, curr); 
+            if (res != Integer.MAX_VALUE) {
+                min = Math.min(min, res + 1);
+            }
+        } 
+        memo.put(amount, min);
+        return min; 
+    }
+}
