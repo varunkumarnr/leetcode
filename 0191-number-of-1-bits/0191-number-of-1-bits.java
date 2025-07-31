245e402ca6f05477d7ac332f6f459e48117152ed
Varun Kumar N.R <varunnrk@gmail.com>
2025-07-31 17:20:40 +0530

    Time: 0 ms (100%), Space: 40.6 MB (72.05%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -2,7 +2,15 @@ class Solution {
     public int hammingWeight(int n) {
         int count = 0; 
        while(n!=0) {
-          // 
+          // compare n with 1 with and biwise operator
+          /* 
+            000011
+            000001
+            000001 == 1
+            if we and 1 then we will know the last bit is one 
+
+            then right shift to remove last bit and compare second last till n beocmes 0 damn
+          */
           if((n & 1) == 1) {
             count++; 
           } 
