821fff71d5cfa3b95a367a49b53a29ed646459ef
Varun Kumar N.R <varunnrk@gmail.com>
2025-08-01 13:49:05 +0530

    Time: 0 ms (100%), Space: 45.8 MB (12.89%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,11 +3,11 @@ class Solution {
         int allSum = 0; 
         int givenSum =0;
         for(int i = 0; i<=nums.length;i++) {
-            allSum += i; 
+            allSum ^= i; 
         }
         for(int j=0; j<nums.length;j++) {
-            givenSum +=nums[j]; 
+            givenSum ^=nums[j]; 
         }
-        return allSum-givenSum; 
+        return allSum^givenSum; 
     }
 }
