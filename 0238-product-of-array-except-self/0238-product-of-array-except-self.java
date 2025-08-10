7d2faa79b2e58e4c2fc1cb45bca7aa82b47290df
Varun Kumar N.R <varunnrk@gmail.com>
2025-08-10 20:01:51 +0530

    Time: 4 ms (5.76%), Space: 56.4 MB (15.63%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,36 +1,20 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        // left prefix product 
-        // [1,2,3,4]
-
-        // i would get 
-
-        // [1, 1, 2,6] 
-
-        // right prefix product 
-
-        // [24,12,4,1]
-        // Set<Integer> seen  = new HashSet<Integer>();
-        int[] leftPrefix = new int[nums.length]; 
-        for(int i = 0; i< nums.length; i++) { 
-            if(i == 0) {
-                leftPrefix[i] = 1;
-                continue;
-            }
-            leftPrefix[i] =  nums[i-1] * leftPrefix[i-1]; 
+    public int[] productExceptSelf(int[] nums) {  
+        int totalProduct = 1;
+        boolean zeroExists = false; 
+        int[] prefix = new int[nums.length];
+        Arrays.fill(prefix, 1);  
+        int[] suffix = new int[nums.length];
+        Arrays.fill(suffix, 1);  
+        for(int i =1; i<nums.length;i++) {
+            prefix[i] = nums[i-1] * prefix[i-1];
         }
-        int[] rightPrefix = new int[nums.length]; 
-        for(int i = nums.length-1 ; i>=0; i--) {
-            if(i == nums.length-1) {
-                rightPrefix[i] = 1; 
-                continue; 
-            }
-            rightPrefix[i] = nums[i+1] * rightPrefix[i+1]; 
+        for(int i = nums.length-2; i>=0; i--) {
+            suffix[i] =  nums[i+1] * suffix[i+1]; 
         }
-        int output[] = new int[nums.length]; 
-        for(int i =0; i< nums.length; i++) {
-            output[i] = leftPrefix[i] * rightPrefix[i];
+        for(int i =0; i < nums.length; i++) {
+            nums[i] = prefix[i] * suffix[i];
         }
-        return output; 
+        return nums;
     }
 }
