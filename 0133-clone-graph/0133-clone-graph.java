a62a3077dea2a631a77dc93ef9c50f3f55ff66bf
Varun Kumar N.R <varunnrk@gmail.com>
2025-08-12 17:30:47 +0530

    Time: 34 ms (7.08%), Space: 42.8 MB (73.54%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -18,24 +18,49 @@ class Node {
 }
 */
 
-class Solution {
+// class Solution {
+//     public Node cloneGraph(Node node) {
+//         if(node == null) return node; 
+//         Queue<Node> queue = new LinkedList<>();
+//         Map<Node, Node> map = new HashMap<>();
+//         queue.add(node);   
+//         map.put(node, new Node(node.val)); 
+//         while(!queue.isEmpty()) {
+//             Node curr = queue.poll(); 
+//             for(Node neighbor: curr.neighbors) {
+//                 if(!map.containsKey(neighbor)) {
+//                     queue.add(neighbor);
+//                     map.put(neighbor, new Node(neighbor.val));
+//                 }
+//                 map.get(curr).neighbors.add(map.get(neighbor));
+//             }     
+//         }
+//         return map.get(node); 
+//     }
+// }
+
+class Solution { 
+    HashMap<Node, Node> map = new HashMap<>();
     public Node cloneGraph(Node node) {
-        Node clone = new Node();
-        if(node == null) return node; 
-        Queue<Node> queue = new LinkedList<>();
-        Map<Node, Node> map = new HashMap<>();
-        queue.add(node);   
-        map.put(node, new Node(node.val)); 
-        while(!queue.isEmpty()) {
-            Node curr = queue.poll(); 
-            for(Node neighbor: curr.neighbors) {
-                if(!map.containsKey(neighbor)) {
-                    queue.add(neighbor);
-                    map.put(neighbor, new Node(neighbor.val));
-                }
-                map.get(curr).neighbors.add(map.get(neighbor));
-            }     
+        if(node == null) return null;
+        dfs(node); 
+        for(Map.Entry<Node, Node> entry : map.entrySet()) {
+            Node curr = entry.getKey(); 
+            Node newCurr = entry.getValue();
+            newCurr.neighbors = new ArrayList<>();
+            for(Node neighbor : curr.neighbors) {
+                newCurr.neighbors.add(map.get(neighbor));
+            }
         }
-        return map.get(node); 
+        return map.get(node);
+    }
+    void dfs(Node node) {
+        if(map.containsKey(node)) {
+            return; 
+        }
+        map.put(node, new Node(node.val));
+        for(Node neigh: node.neighbors) {
+            dfs(neigh);
+        }
     }
 }
