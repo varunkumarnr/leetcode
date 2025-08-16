24c8a2de7df2fd4187f5974c6502f06e267d12f4
Varun Kumar N.R <varunnrk@gmail.com>
2025-08-16 08:12:42 +0530

    Time: 2 ms (83.17%), Space: 44.7 MB (63.96%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -5,16 +5,16 @@ class Solution {
         return result; 
     }
     void backtracking(int[] candidates, int target, List<Integer> curr, int start) {
-        int sum = curr.stream().mapToInt(Integer::intValue).sum();
-        if(sum == target) {
+        // int sum = curr.stream().mapToInt(Integer::intValue).sum();
+        if(target == 0) {
             result.add(new ArrayList<>(curr));
         }
-        if(target < sum) {
+        if(target < 0) {
             return; 
         }
         for(int i = start; i < candidates.length; i++) {
             curr.add(candidates[i]); 
-            backtracking(candidates, target, curr, i); 
+            backtracking(candidates, target - candidates[i], curr, i); 
             curr.remove(curr.size()-1);
         }
     }
