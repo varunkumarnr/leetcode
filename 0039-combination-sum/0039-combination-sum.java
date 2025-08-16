989017009520c2769d27ff754901d109dbe161fe
Varun Kumar N.R <varunnrk@gmail.com>
2025-08-16 08:10:05 +0530

    Time: 17 ms (5.16%), Space: 45.4 MB (5.82%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,26 +1,21 @@
 class Solution {
+    List<List<Integer>> result  = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>(); 
-        Arrays.sort(candidates);
-        backtracking(result,candidates , new ArrayList<>(), target, 0, 0); 
+        backtracking(candidates, target, new ArrayList<>(), 0); 
         return result; 
     }
-    void backtracking(List<List<Integer>> result, int[] candidates, List<Integer> curr, int target, int start, int currSum) {
-        if(currSum == target) { 
-            result.add(new ArrayList<>(curr)); 
+    void backtracking(int[] candidates, int target, List<Integer> curr, int start) {
+        int sum = curr.stream().mapToInt(Integer::intValue).sum();
+        if(sum == target) {
+            result.add(new ArrayList<>(curr));
+        }
+        if(target < sum) {
             return; 
         }
-        for(int i = start; i<candidates.length; i++) {
-            currSum += candidates[i];
-            curr.add(candidates[i]);
-            if(currSum > target) {
-                int last = curr.remove(curr.size() - 1);
-                currSum -= last;
-                continue;
-            }
-            backtracking(result , candidates, curr, target, i, currSum); 
-            int last = curr.remove(curr.size() - 1);
-            currSum -= last;
+        for(int i = start; i < candidates.length; i++) {
+            curr.add(candidates[i]); 
+            backtracking(candidates, target, curr, i); 
+            curr.remove(curr.size()-1);
         }
-    } 
+    }
 }
