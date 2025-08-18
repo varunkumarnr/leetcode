a800427afd6b57cb944e893d0809ac3b3dc07f69
Varun Kumar N.R <varunnrk@gmail.com>
2025-08-18 14:39:48 +0530

    Time: 1 ms (9.07%), Space: 42.4 MB (80.4%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,27 +1,41 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || head.next == null || k == 0) return head;
-
-        // Find length and connect tail to head (circular list)
-        int length = 1;
-        ListNode tail = head;
-        while (tail.next != null) {
-            length++;
-            tail = tail.next;
+        if(head == null) return head;
+        ListNode temp = head; 
+        int size = 0;
+        while(temp != null) {
+            temp = temp.next;
+            size++; 
         }
-        tail.next = head; // circle
-
-        // Find new head after rotation
-        k = k % length;
-        int stepsToNewHead = length - k;
-        ListNode newTail = tail;
-        while (stepsToNewHead-- > 0) {
-            newTail = newTail.next;
+        ListNode trev = head;
+        Queue<ListNode> stack = new LinkedList<>();
+        ListNode ending = new ListNode(0);
+        ListNode point = ending; 
+        int rev = size > k ? size - k : size - (k % size);  
+            while(rev > 0) {
+                ending.next = trev;
+                ending = ending.next;
+                trev = trev.next;  
+                rev--; 
+            }   
+            ending.next = null; 
+            while(trev != null) {
+                stack.offer(trev);    
+                trev = trev.next; 
+            }
+        ListNode remaining = point.next; 
+        ListNode dummy = new ListNode(0);
+        ListNode temp1 = dummy;
+        while(!stack.isEmpty()) {
+            ListNode curr = stack.poll(); 
+            temp1.next = curr; 
+            temp1 = temp1.next; 
+        }      
+        while(remaining != null) {
+            temp1.next = remaining; 
+            temp1 = temp1.next;
+            remaining  = remaining.next;
         }
-
-        ListNode newHead = newTail.next;
-        newTail.next = null; // break circle
-
-        return newHead;
+        return dummy.next; 
     }
 }
