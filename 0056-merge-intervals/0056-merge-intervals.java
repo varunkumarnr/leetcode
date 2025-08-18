305cfb9ac64431117a41350df159e0c0fa68583f
Varun Kumar N.R <varunnrk@gmail.com>
2025-08-18 09:18:14 +0530

    Time: 10 ms (20.92%), Space: 46.5 MB (73.19%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,25 +1,40 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-        List<int[]> results = new ArrayList<>();
-        int[] temp = new int[]{-1,-1}; 
-        for(int[] interval : intervals) {
-            if(temp[1] != -1 && temp[1] < interval[0]) 
-            {
-                results.add(new int[]{temp[0], temp[1]});
-                temp[0] = -1;
-                temp[1] = -1; 
-            }; 
-            temp[1] = Math.max(temp[1], interval[1]);    
-            if(temp[0] == -1) temp[0] = interval[0]; 
+        List<int[]> merged = new ArrayList<>();
+        if(intervals.length <= 1) return intervals; 
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        for(int i = 0; i<intervals.length-1; i++) {
+            int[] curr = intervals[i]; 
+            int start = curr[0];
+            int end = curr[1]; 
+            while(i<intervals.length-1 && curr[1] >= intervals[i+1][0]) {
+                if(end < intervals[i+1][1]) {
+                    curr = intervals[i+1]; 
+                    end = intervals[i+1][1]; 
+                }
+                i++; 
+            }
+            merged.add(new int[]{start, end});
         }
-        if(temp[0] != -1 && temp[1] !=-1) {
-            results.add(temp); 
-        } 
-        int[][] res = new int[results.size()][2];
-        for(int i= 0; i<results.size(); i++){
-            res[i] = results.get(i); 
+        // add last element
+        if(merged.get(merged.size()-1)[1] != intervals[intervals.length-1][1]) {
+            if(merged.get(merged.size()-1)[1] >= intervals[intervals.length-1][0]) {
+                int[] temp = merged.get(merged.size()-1);
+                int start = temp[0];
+                int end = temp[1];
+                if(end < intervals[intervals.length-1][1]) {
+                    merged.remove(merged.size()-1); 
+                    merged.add(new int[]{start, intervals[intervals.length-1][1]});
+                }
+            } else {
+                merged.add(new int[]{intervals[intervals.length-1][0], intervals[intervals.length-1][1]});
+            }
         }
-        return res; 
+
+        int[][] result = new int[merged.size()][2];
+        for(int i=0; i<merged.size();i++) {
+            result[i] = merged.get(i);
+        }
+        return result;
     }
 }
