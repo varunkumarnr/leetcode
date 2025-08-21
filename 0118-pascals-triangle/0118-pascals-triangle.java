8a2f39b12989082133ac46de23a207de3b330a7a
Varun Kumar N.R <varunnrk@gmail.com>
2025-08-21 10:28:27 +0530

    Time: 1 ms (86.23%), Space: 42.2 MB (40.15%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,47 +1,18 @@
-// class Solution {
-//     public List<List<Integer>> generate(int numRows) {
-//         List<List<Integer>> result = new ArrayList<>();
-//         List<Integer> prev  = new ArrayList<>(); 
-//         List<Integer> curr = new ArrayList<>(); 
-//         prev.add(1); 
-//         result.add(new ArrayList<>(prev)); 
-//         for(int i = 2; i<= numRows;i++) {
-//             for(int j = 0; j<i;j++) {
-//                 int currNum = 0;
-//                 if(j==0) {
-//                     currNum += prev.get(j);
-//                 } else if(j==i-1) {
-//                     currNum+=prev.get(j-1); 
-//                 }else {
-//                     currNum = prev.get(j-1) +prev.get(j);
-//                 }
-//                 curr.add(currNum);
-//             }
-//             result.add(new ArrayList<>(curr));
-//             prev.clear();
-//             prev.addAll(curr);
-//             curr.clear();
-//         }
-//         return result; 
-//     }
-// }
-
-
-
-class Solution{
-    public List<List<Integer>> generate(int n){
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            List<Integer> temp=new ArrayList<>();
-            for(int j=0;j<=i;j++){
-                if(j==0 || j==i)
-                    temp.add(1);
-                else{
-                    temp.add(ans.get(i-1).get(j-1) + ans.get(i-1).get(j));
-                }
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0; i< numRows; i++) {
+           List<Integer> curr = new ArrayList<>(); 
+           for (int j =0; j<=i; j++) {
+            if(j == 0 || j==i) {
+                curr.add(1);
             }
-            ans.add(temp);
+            else{
+                curr.add(result.get(i-1).get(j-1) + result.get(i-1).get(j));
+           } 
+           }
+           result.add(curr);
         }
-        return ans; 
+        return result; 
     }
 }
