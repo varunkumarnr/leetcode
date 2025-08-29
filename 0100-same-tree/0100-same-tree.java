d97e83eb0d38c8bfdae2737f19e63e1f440f76b6
Varun Kumar N.R <varunnrk@gmail.com>
2025-08-29 14:32:41 +0530

    Time: 0 ms (100%), Space: 41 MB (69.65%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -13,52 +13,14 @@
  *     }
  * }
  */
-// class Solution {
-//     public boolean isSameTree(TreeNode p, TreeNode q) {
-//         Queue<TreeNode> queueP = new LinkedList<>(); 
-//         Queue<TreeNode> queueQ = new LinkedList<>(); 
-//         if(p == null && q != null) return false; 
-//         if(q == null && p!= null) return false;
-//         if(p== null && q==null) return true;
-//         queueP.offer(p); 
-//         queueQ.offer(q); 
-//         while(!queueP.isEmpty() || !queueQ.isEmpty()) {
-//             int n = queueP.size(); 
-//             while(n>0) {
-//             TreeNode currP = queueP.poll();
-//             TreeNode currQ = queueQ.poll();
-//             if(currP.val != currQ.val) return false;
-//             if(currP.left != null) queueP.offer(currP.left); 
-//             if(currQ.left != null) queueQ.offer(currQ.left); 
-//             if(queueP.size() != queueQ.size()) {
-//                 return false;
-//             }
-//             if(currP.right != null) queueP.offer(currP.right); 
-//             if(currQ.right != null) queueQ.offer(currQ.right);
-//             n--;
-//             if(queueP.size() != queueQ.size()) {
-//                 return false;
-//             }
-//             } 
-//         }
-//         return true; 
-//     }
-// }
-
-
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return dfs(p,q); 
+        return dfs(p, q); 
     }
-
     boolean dfs(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true; 
-         if (p == null || q == null) return false;
+        if(p == null && q== null) return true; 
+        if(p == null || q == null) return false; 
         if(p.val != q.val) return false; 
-        boolean left = dfs(p.left, q.left); 
-        if(left == false) return false;
-        boolean right = dfs(p.right, q.right);
-        if(right == false) return false;         
-        return left && right; 
+        return dfs(p.left, q.left) && dfs(p.right, q.right); 
     }
 }
