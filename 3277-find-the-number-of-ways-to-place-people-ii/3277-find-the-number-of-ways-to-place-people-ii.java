d937c0981cdf48de8a403742b1ba356e68d7df32
Varun Kumar N.R <varunnrk@gmail.com>
2025-09-03 09:08:18 +0530

    Time: 383 ms (37.68%), Space: 45.4 MB (43.48%) - LeetHub

--- 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.java
+++ 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.java
@@ -4,7 +4,6 @@ class Solution {
             if (a[0] != b[0]) return b[0] - a[0];
             else return a[1] - b[1];
         });
-        System.out.println(Arrays.deepToString(points));
         int count = 0; 
         for(int i = 0; i<points.length; i++) {
             for(int j =i+1; j<points.length; j++) {                
