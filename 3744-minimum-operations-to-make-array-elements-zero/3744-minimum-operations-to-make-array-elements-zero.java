bd5f36812e3323fcd9ae86abdd931637c64b53e0
Varun Kumar N.R <varunnrk@gmail.com>
2025-09-06 12:25:04 +0530

    Time: 36 ms (16.67%), Space: 100.9 MB (77.78%) - LeetHub

--- 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.java
+++ 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.java
@@ -1,3 +1,45 @@
+// class Solution {
+//     public long minOperations(int[][] nums) {
+//         /*
+//             1 , 2, 3, 4, 5, 6, 7,  8
+//             1  1 1   2   2   2  2  2 13/ 2 = 7 
+        
+        
+//          */
+//         List<List<Integer>> input = new ArrayList(); 
+//         long result = 0; 
+//         for(int i =0; i < nums.length; i++) {
+//             List<Integer> curr = new ArrayList<>(); 
+//             for(int j = nums[i][0] ; j <= nums[i][1]; j++) {
+//                 curr.add(j); 
+//             }
+//             input.add(curr); 
+//         }
+//         HashMap<Integer, Integer> map = new HashMap<>(); 
+//         for(List<Integer> i: input) {
+//             long countTotal = 0; 
+//             for(int k: i ) {
+//                 int temp = k; 
+//                 int count = 0; 
+//                 if(map.containsKey(k)){ 
+//                     count = map.get(k); 
+//                     countTotal += count; 
+//                     continue; 
+//                 }
+//                 while(k>0) {
+//                     k = (int) Math.floor((double) k/4); 
+//                     count++; 
+//                 }
+//                 countTotal += count; 
+//                 map.put(temp, count); 
+//             }
+//             result += (countTotal + 1) / 2;
+//         }
+//         return result; 
+//     }
+// }
+
+
 class Solution {
     public long minOperations(int[][] nums) {
         long result = 0L;
