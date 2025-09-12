30385fd0bfaccc84b78a7537d2d87901f00e99b9
Varun Kumar N.R <varunnrk@gmail.com>
2025-09-12 07:46:35 +0530

    Time: 4 ms (79.79%), Space: 45.5 MB (82.68%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -2,12 +2,10 @@ class Solution {
     public boolean doesAliceWin(String s) {
         int countVowels = 0; 
         for(char c: s.toCharArray()) { 
-            c = Character.toLowerCase(c); 
             if(c == 'a' || c== 'e' || c== 'i'|| c=='o' || c=='u') {
                 return true;
             }
         }
         return false; 
-        
     }
 }
