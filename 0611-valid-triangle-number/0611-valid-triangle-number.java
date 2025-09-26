34ef98c2eda6eabb40964962c838ad4aaddbd44a
Varun Kumar N.R <varunnrk@gmail.com>
2025-09-26 10:23:02 +0530

    Time: 25 ms (35.42%), Space: 43.1 MB (92.68%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -1,5 +1,11 @@
 class Solution {
     public int triangleNumber(int[] nums) {
+
+        /* 
+            2  2 3 4   2 + 3 > 4
+            i    j  k    result = j- i
+            if wil be grater then obviously 1th element will be greater instead of recomuting just add. 
+        */
         int result = 0; 
         Arrays.sort(nums); 
         for (int i = nums.length - 1; i >= 2; i--) { 
