1a47b2401d9a5ed81669213e31b8666eee5e1216
Varun Kumar N.R <varunnrk@gmail.com>
2026-03-24 10:27:35 +0530

    Time: 14 ms (3.24%), Space: 44.8 MB (81.07%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        if(points.length < 2) return 0; 
+        int curr_x = points[0][0]; 
+        int curr_y = points[0][1]; 
+        int total_steps = 0; 
+        for(int[] point : points) { 
+            int x = point[0]; 
+            int y = point[1]; 
+            // if(curr_x == x && curr_y == y) continue; 
+            while(curr_x != x || curr_y != y) { 
+                // move diagonally as much as possible 
+                // chose which diagonal 
+                if(curr_x == x) {
+                    if(curr_y < y) { 
+                        curr_y +=1; 
+                        total_steps +=1; 
+                    } else { 
+                        curr_y -= 1; 
+                        total_steps +=1; 
+                    }
+                } // only move up or down
+                else if(curr_y == y) {
+                    if(curr_x < x) { 
+                        curr_x +=1; 
+                        total_steps +=1; 
+                    } else { 
+                        curr_x -= 1; 
+                        total_steps +=1; 
+                    }
+                } // only move left or right
+                else if(curr_x > x && curr_y > y) { // move bottom left
+                    curr_x--; 
+                    curr_y--; 
+                    total_steps +=1; 
+                } else if ( curr_x  < x && curr_y >y) // move bottom right
+                    {
+                        curr_x++; 
+                        curr_y--; 
+                        total_steps++; 
+                }  else if (curr_x < x && curr_y <y) {  // top right
+                    curr_x++; 
+                    curr_y++; 
+                    total_steps++; 
+                 }    else    if( curr_x > x && curr_y < y) // top left
+                 { 
+                    curr_x--;
+                    curr_y++; 
+                    total_steps++; 
+                 }      
+                }
+        }
+        return total_steps; 
+    }
+}
